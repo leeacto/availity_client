7e89ef36dd9d609d37ddb186d8795ab171acefd6
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-05-12 09:01:50 -0500

    move request to common method

--- lib/availity_client.rb
+++ lib/availity_client.rb
@@ -7,4 +7,23 @@ module AvailityClient
   BASE_URL = "https://api.availity.com/#{"demo/" unless ENV['RACK_ENV'] == 'production'}v1/"
 
   class MissingApiKey < StandardError; end
+
+  def self.issue_request(method, url, params)
+    api_key = ENV['AVAILITY_API_KEY']
+    raise MissingApiKey, "AVAILITY_API_KEY Env Var must be set" unless api_key
+
+    uri = URI.parse(url)
+    conn = Faraday.new(url: uri) do |faraday|
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+
+    response = conn.get do |req|
+      req.headers['x-api-key'] = api_key
+
+      if params.any?
+        params.first.each { |key, value| req.params[key.to_s] = value }
+      end
+    end
+  end
 end
--- lib/availity_client/coverage.rb
+++ lib/availity_client/coverage.rb
@@ -2,24 +2,10 @@ module AvailityClient
   class Coverage
     class << self
       def get(* args)
-        api_key = ENV['AVAILITY_API_KEY']
-        raise MissingApiKey, "AVAILITY_API_KEY Env Var must be set" unless api_key
         id = args.shift.to_s if args.first.kind_of?(Integer)
         url = "#{BASE_URL}coverages#{"/" + id if id}" 
 
-        uri = URI.parse(url)
-        conn = Faraday.new(url: uri) do |faraday|
-          faraday.response :logger
-          faraday.adapter  Faraday.default_adapter
-        end
-
-        response = conn.get do |req|
-          req.headers['x-api-key'] = api_key
-
-          if args.any?
-            args.first.each { |key, value| req.params[key.to_s] = value }
-          end
-        end
+        response = AvailityClient.issue_request('get', url, args)
 
         {
           status: response.status,
