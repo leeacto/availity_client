fdeedff8501ce5bd007424b29654d1c442c07165
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-05-23 15:19:24 -0500

    test to generate oauth token

--- lib/availity_client.rb
+++ lib/availity_client.rb
@@ -2,39 +2,62 @@ require 'availity_client/coverage'
 require 'faraday'
 require 'json'
 require 'uri'
+require 'base64'
 
 module AvailityClient
-  BASE_URL = "https://api.availity.com/#{"demo/" unless ENV['RACK_ENV'] == 'production'}v1/"
+  BASE_URL = "https://api.availity.com/#{"demo/" unless ENV['AVAILITY_API_SECRET'] == 'production'}v1/"
 
   class MissingApiKeyError < StandardError; end
 
-  def self.issue_request(method, url, params)
-    api_key = ENV['AVAILITY_API_KEY']
-    raise MissingApiKeyError, "AVAILITY_API_KEY Env Var must be set" unless api_key
-
-    uri = URI.parse(url)
-    conn = Faraday.new(url: uri) do |faraday|
-      faraday.response :logger
-      faraday.adapter  Faraday.default_adapter
+  class << self
+    def premium_key?
+      !!ENV['AVAILITY_API_KEY'] && !!ENV['AVAILITY_API_SECRET']
     end
 
-    response = conn.send(method.to_sym) do |req|
-      req.headers['x-api-key'] = api_key
-
-      if params.any?
-        params.first.each { |key, value| req.params[key.to_s] = value }
+    def generate_token
+      base_64_token = Base64.encode64(ENV['AVAILITY_API_KEY'] + ":" + ENV['AVAILITY_API_SECRET']).chomp
+      conn = Faraday.new(url: "https://api.availity.com") do |faraday|
+        faraday.response :logger
+        faraday.adapter  Faraday.default_adapter
       end
+
+      response = conn.post do |req|
+        req.url '/v1/token'
+        req.headers['Authorization'] = "Basic #{base_64_token}"
+        req.body = "grant_type=client_credentials"
+      end
+
+      JSON.parse(response.body)['access_token']
     end
 
-    response_body = begin
-                      JSON.parse(response.body)
-                    rescue JSON::ParserError => e
-                      response.body
-                    end
-    {
-      headers: response.headers,
-      status: response.status,
-      body: response_body
-    }
+    def issue_request(method, url, params)
+      api_key = ENV['AVAILITY_API_KEY']
+      raise MissingApiKeyError, "AVAILITY_API_KEY Environment Variable must be set" unless api_key
+
+      uri = URI.parse(url)
+      conn = Faraday.new(url: uri) do |faraday|
+        faraday.response :logger
+        faraday.adapter  Faraday.default_adapter
+      end
+
+      response = conn.send(method.to_sym) do |req|
+        req.headers['x-api-key'] = api_key
+
+        if params.any?
+          params.first.each { |key, value| req.params[key.to_s] = value }
+        end
+      end
+
+      response_body = begin
+                        JSON.parse(response.body)
+                      rescue JSON::ParserError => e
+                        response.body
+                      end
+      {
+        headers: response.headers,
+        status: response.status,
+        body: response_body
+      }
+    end
   end
 end
--- spec/lib/availity_client_spec.rb
+++ spec/lib/availity_client_spec.rb
@@ -1,31 +1,68 @@
 require 'spec_helper'
 
-describe ".issue_request" do
-  before do
-    ENV['AVAILITY_API_KEY'] = 'fakeApiKeyHere'
+describe AvailityClient do
+  describe ".premium_key?" do
+    it "returns true if AVAILITY_API_KEY and AVAILITY_API_SECRET variables are present" do
+      ENV['AVAILITY_API_KEY'] = 'fakeApiKeyHere'
+      ENV['AVAILITY_API_SECRET'] = 'secretKey'
+      expect(AvailityClient.premium_key?).to be true
+    end
+
+    it "returns false if AVAILITY_API_SECRET variable is missing" do
+      ENV['AVAILITY_API_KEY'] = 'fakeApiKeyHere'
+      ENV['AVAILITY_API_SECRET'] = nil
+      expect(AvailityClient.premium_key?).to be false
+    end
+
+    it "returns false if AVAILITY_API_SECRET variable is missing" do
+      ENV['AVAILITY_API_KEY'] = nil
+      ENV['AVAILITY_API_SECRET'] = nil
+      expect(AvailityClient.premium_key?).to be false
+    end
   end
 
-  it "raises an error without AVAILITY_API_KEY Env Var" do
-    ENV['AVAILITY_API_KEY'] = nil
+  describe ".generate_token" do
+    it "returns an Availity Oauth Token" do
+      ENV['AVAILITY_API_KEY'] = 'fakeApiKeyHere'
+      ENV['AVAILITY_API_SECRET'] = 'secretKey'
+      response = double(
+                   body:  {"access_token" => "cmj7kkkhc2zmwv9ekseptneg","token_type" => "bearer","expires_in" => 300}.to_json,
+                   status: 200)
 
-    expect{
-      AvailityClient.issue_request('get', 'http://example.com', {})
-    }.to raise_error MissingApiKeyError
+      faraday = double()
+      expect(Faraday).to receive(:new).and_return faraday
+      expect(faraday).to receive(:post).and_return response
+      expect(AvailityClient.generate_token).to eq 'cmj7kkkhc2zmwv9ekseptneg'
+    end
   end
 
-  it "issues request based on provided parameters" do
-    method = 'post'
-    url = 'http://example.com'
-    params = { foo: 'bar' }
+  describe ".issue_request" do
+    before do
+      ENV['AVAILITY_API_KEY'] = 'fakeApiKeyHere'
+    end
 
-    expect(URI).to receive(:parse).with(url).and_call_original
-    allow_any_instance_of(Faraday::Connection).to receive(:post)
-      .and_return(OpenStruct.new(status: 200, body: { foo: 'bar' }.to_json), headers: { "x-response" => "ok" })
+    it "raises an error without AVAILITY_API_KEY Env Var" do
+      ENV['AVAILITY_API_KEY'] = nil
 
-    response = AvailityClient.issue_request(method, url, params)
+      expect{
+        AvailityClient.issue_request('get', 'http://example.com', {})
+      }.to raise_error MissingApiKeyError
+    end
 
-    expect(response).to have_key(:headers)
-    expect(response[:status]).to eq 200
-    expect(response[:body]).to eq({ 'foo' => 'bar' })
+    it "issues request based on provided parameters" do
+      method = 'post'
+      url = 'http://example.com'
+      params = { foo: 'bar' }
+
+      expect(URI).to receive(:parse).with(url).and_call_original
+      allow_any_instance_of(Faraday::Connection).to receive(:post)
+        .and_return(OpenStruct.new(status: 200, body: { foo: 'bar' }.to_json), headers: { "x-response" => "ok" })
+
+      response = AvailityClient.issue_request(method, url, params)
+
+      expect(response).to have_key(:headers)
+      expect(response[:status]).to eq 200
+      expect(response[:body]).to eq({ 'foo' => 'bar' })
+    end
   end
 end
